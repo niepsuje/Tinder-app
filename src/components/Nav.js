188dae55a5d54bbf7289bbbb4488330d64c64220
niepsuje <jakubniepsujj@gmail.com>
2023-08-20 11:37:31 +0200

    2. Homepage UI

---
+++ src/components/Nav.js
@@ -0,0 +1,22 @@
+import whiteLogo from '../images/tinder_logo_white.png'
+import colorLogo from '../images/color-logo-tinder.png'
+
+
+const Nav = ({ minimal, authToken}) => {
+
+
+
+    return (
+        <nav>
+            <div className="logo-container">
+                <img className="logo" src={minimal ? colorLogo : whiteLogo}/>
+            </div>
+
+            {!authToken && !minimal && <button className="nav-button">Log in</button>}
+        </nav>
+
+
+    )
+}
+
+export default Nav
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,24 @@
+import Nav from '../components/Nav'
+
 const Home = () => {
+
+    const authToken = false
+
+    const handleClick = () => {
+        console.log('clocked')
+    }
+
     return (
-        <div>Home</div>
+        <div className='overlay'>
+        <Nav minimal={false} authToken={authToken}/>
+        <div className="home">
+            <h1>Swipe Right@</h1>
+            <button className="primary-button" onClick={handleClick}>
+                {authToken ? 'Signout' : "Create Account"}
+            </button>
+
+        </div>
+        </div>
     )
 }
 
