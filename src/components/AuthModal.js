d145714c4f1c8a086dad4c6a335d5b58e480b2cb
niepsuje <jakubniepsujj@gmail.com>
2023-08-22 14:20:08 +0200

    5. Homepage UI (3)

--- src/components/AuthModal.js
+++ src/components/AuthModal.js
@@ -1,14 +1,12 @@
 import { useState } from 'react'
 
-const AuthModal = ({ setShowModal }) => {
+const AuthModal = ({ setShowModal, setIsSignUp, isSignUp }) => {
     const[ email, setEmail] = useState(null)
     const[ password, setPassword] = useState(null)
     const[ confirmPassword, setConfirmPassword] = useState(null)
     const[ error, setError] = useState(null)
 
     console.log(email, password, confirmPassword)
-
-    const isSignUp = true
 
 
     const handleClick = () => {
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,28 +5,34 @@ import {  useState } from 'react'
 const Home = () => {
 
     const [showModal, setShowModal] = useState(false)
+    const [isSignUp, setIsSignUp] = useState(true)
 
     const authToken = false
 
     const handleClick = () => {
         console.log('clocked')
         setShowModal(true)
+        setIsSignUp(true)
     }
 
     return (
         <div className='overlay'>
-        <Nav minimal={false} authToken={authToken} setShowModal={setShowModal} showModal={showModal}/>
-        <div className="home">
-            <h1>Swipe Right@</h1>
-            <button className="primary-button" onClick={handleClick}>
-                {authToken ? 'Signout' : "Create Account"}
-            </button>
+            <Nav minimal={false} 
+                authToken={authToken} 
+                setShowModal={setShowModal} 
+                showModal={showModal}
+                setIsSignUp={setIsSignUp}/>
+            <div className="home">
+                <h1>Swipe Right@</h1>
+                <button className="primary-button" onClick={handleClick}>
+                    {authToken ? 'Signout' : "Create Account"}
+                </button>
 
-            {showModal && (
-                <AuthModal setShowModal={setShowModal}/>
-            )}
+                {showModal && (
+                    <AuthModal setShowModal={setShowModal} isSignUp={isSignUp} />
+                )}
 
-        </div>
+            </div>
         </div>
     )
 }
