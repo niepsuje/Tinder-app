3dba91a3ba6b64851bb11f0a164061f286f4d2f8
niepsuje <jakubniepsujj@gmail.com>
2023-08-29 17:43:29 +0200

    16. ChatContainer UI (3)

---
+++ client/src/components/Nav.js
@@ -0,0 +1,30 @@
+import whiteLogo from '../images/tinder_logo_white.png'
+import colorLogo from '../images/color-logo-tinder.png'
+
+
+const Nav = ({ minimal, setShowModal, showModal, setIsSignUp}) => {
+
+    const handleClick = () => {
+        setShowModal(true)
+        setIsSignUp(false)
+    }
+
+    const authToken = true
+    return (
+        <nav>
+            <div className="logo-container">
+                <img className="logo" src={minimal ? colorLogo : whiteLogo}/>
+            </div>
+
+            {!authToken && !minimal && <button 
+                className="nav-button"
+                onClick={handleClick}
+                disabled={showModal}
+            >Log in</button>}
+        </nav>
+
+
+    )
+}
+
+export default Nav
